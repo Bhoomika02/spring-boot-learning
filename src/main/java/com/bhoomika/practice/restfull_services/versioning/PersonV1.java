1a3df3bc38165dab8d87682a1ece208e26aa9f34
Bhoomika L <bhoomikal@Bhoomikas-MacBook-Air.local>
2026-09-06 01:06:06 +0530

    versioning

---
+++ src/main/java/com/bhoomika/practice/restfull_services/versioning/PersonV1.java
@@ -0,0 +1,20 @@
+package com.bhoomika.practice.restfull_services.versioning;
+
+public class PersonV1 {
+	private String name;
+
+	public PersonV1(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public String toString() {
+		return "PersonV1 [name=" + name + "]";
+	}
+
+}
